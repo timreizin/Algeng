b9d2bb55f31408f26e172f0ce2a6e51f64205fda
Tymofii Reizin <46033117+timreizin@users.noreply.github.com>
2023-08-08 22:56:25 +0200

    Created a Variable class.

---
+++ Variable.cs
@@ -0,0 +1,16 @@
+namespace Algeng;
+
+class Variable
+{
+    private long _value;
+
+    public Variable(long value)
+    {
+        _value = value;
+    }
+
+    public long GetValue()
+    {
+        return _value;
+    }
+}
