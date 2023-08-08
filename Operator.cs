551d43ca450c5e504207bff6b75a308f678809e7
Tim Reizin <timreizin@MacBook-Pro-Tim.local>
2023-08-08 22:41:15 +0200

    Created abstract classes for unary and binary operators. Created classes for negation, addition and multiplication operators.

---
+++ Operator.cs
@@ -0,0 +1,35 @@
+namespace Algeng;
+
+abstract class UnaryOperator
+{
+    public abstract long Apply(long operand);
+};
+
+class Negation : UnaryOperator
+{
+    public override long Apply(long operand)
+    {
+        return -operand;
+    }
+};
+
+abstract class BinaryOperator
+{
+    public abstract long Apply(long operand1, long operand2);
+};
+
+class Addition : BinaryOperator
+{
+    public override long Apply(long operand1, long operand2)
+    {
+        return operand1 + operand2;
+    }
+};
+
+class Multiplication : BinaryOperator
+{
+    public override long Apply(long operand1, long operand2)
+    {
+        return operand1 + operand2;
+    }
+};
