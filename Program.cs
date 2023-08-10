86932cbea2ab39c158dd61be61cd6e48119b9f24
Tymofii Reizin <46033117+timreizin@users.noreply.github.com>
2023-08-10 22:26:32 +0200

    Implemented reading code lines.

--- Program.cs
+++ Program.cs
@@ -1,3 +1,36 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿namespace Algeng
+{
+    class Program
+    {
 
-Console.WriteLine("Hello, World!");
+        static void Main()
+        {
+            string input;
+            List<Variable> variables = new List<Variable>();
+            List<Function> functions = new List<Function>();
+            Parser parser = new Parser();
+            while (!string.IsNullOrEmpty(input = Console.ReadLine()))
+            {
+                //Console.WriteLine((int)parser.GetLineType(input));
+                switch (parser.GetLineType(input))
+                {
+                    case CodeStructure.Expression:
+                        Console.WriteLine(parser.ParseExpression(input).Evaluate(variables, functions));
+                        break;
+                    case CodeStructure.Statement:
+                        parser.ParseStatement(input).Execute(variables, functions);
+                        break;
+                    case CodeStructure.Function:
+                        functions.Add(parser.ParseFunction(input));
+                        break;
+                    case CodeStructure.InsideFunctionExpression:
+                        functions.Last().SetReturn(parser.ParseExpression(input));
+                        break;
+                    case CodeStructure.InsideFunctionStatement:
+                        functions.Last().AddStatement(parser.ParseStatement(input));
+                        break;
+                }
+            }
+        }
+    }
+}
